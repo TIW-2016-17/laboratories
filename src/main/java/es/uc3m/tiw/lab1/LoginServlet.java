0ca9bf306e9fcd0b70d0bf9d0c1e51bc24be6ff0
Tecnologias de Internet para la web <tiw@vagrant.vm>
2016-09-10 19:08:55 +0000

    Exercise7. Sessions

--- src/main/java/es/uc3m/tiw/lab1/LoginServlet.java
+++ src/main/java/es/uc3m/tiw/lab1/LoginServlet.java
@@ -10,27 +10,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Ejercicio4Servlet
  */
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
+	private static final String USERS = "users";
+	private static final String KEY = "key";
+	private static final String NAME = "name";
+	private static final String AUTHENTICATED = "authenticated";
 	private static final String LOGIN_JSP = "/login.jsp";
 	private static final String LISTADO_JSP = "/list.jsp";
 	private static final String ERROR_JSP = "/error.jsp";
 	private static final long serialVersionUID = 1L;
 	private ServletConfig config;
 	private List<String> usersList;
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public LoginServlet() {
 		super();
 	}
-	
+
 	@Override
-	public void init(ServletConfig config) throws ServletException {	
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
 		this.config = config;
 		usersList = new ArrayList<>();
 		usersList.add("John");
@@ -45,7 +52,27 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		config.getServletContext().getRequestDispatcher(LOGIN_JSP).forward(request, response);
+		String page = "";		
+
+		boolean authenticated;
+		HttpSession session = request.getSession();
+
+		if (session.getAttribute(AUTHENTICATED) != null) {
+			authenticated = (boolean) session.getAttribute(AUTHENTICATED);
+		} else {
+			authenticated = false;			
+			page = LOGIN_JSP;
+
+		}
+
+		if (authenticated) {			
+			page = LISTADO_JSP;
+
+		}
+
+		
+		config.getServletContext().getRequestDispatcher(page).forward(request, response);
+		
 
 	}
 
@@ -54,22 +81,27 @@ public class LoginServlet extends HttpServlet {
 	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String name = request.getParameter("name");
-		String password = request.getParameter("key");
+			throws ServletException, IOException {		
+		String page = LISTADO_JSP;
 
-		String pagina = "";
+		HttpSession session = request.getSession();
 
-		if (name.equals("user1") || password.equals("password1")) {
+		String name = request.getParameter(NAME);
+		String password = request.getParameter(KEY);
 
-			pagina = LISTADO_JSP;
-			request.setAttribute("users", usersList);
+		if (name.equals("1") || password.equals("1")) {
+			session.setAttribute(AUTHENTICATED, true);
+			request.setAttribute(USERS, usersList);
 
 		} else {
-			pagina = ERROR_JSP;
+
+			session.setAttribute(AUTHENTICATED, false);
+			page = ERROR_JSP;
 
 		}
-		config.getServletContext().getRequestDispatcher(pagina).forward(request, response);
+
+		config.getServletContext().getRequestDispatcher(page).forward(request, response);
+
 	}
 
 }
